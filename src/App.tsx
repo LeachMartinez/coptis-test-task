2c7ebc5c632196f6713fef5180e313247c67fd7e
Сунсин Андрей Витальевич <snsn@callibri.ru>
2024-08-21 13:48:17 +0500

    тестове задание коптис

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/header"
+import Map from "./components/map"
+import "./styles/main.scss";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Map/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/map/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+import { Map as OlMap, View} from "ol";
+import styles from "./map.module.scss";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import * as olProj from 'ol/proj';
+import { defaults } from 'ol/interaction/defaults';
+
+const Map: React.FC = () => {  
+  const mapElement = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<OlMap>();
+
+  useEffect(() => {
+    if (mapElement.current && mapRef.current) return;
+    mapRef.current = new OlMap({
+      interactions: defaults({mouseWheelZoom:false}),
+      layers: [raster],
+      target: mapElement.current || "",
+      view
+    });
+  }, []);
+
+  const proj = olProj.get('EPSG:3857');
+  if (proj === null) return;
+
+  const extent = proj.getExtent().slice();
+  extent[0] += extent[0];
+  extent[2] += extent[2];
+
+  const raster = new TileLayer({
+    preload: Infinity,
+    source: new OSM(),
+  })
+
+  const view = new View({
+    center: [11000000, 8600000],
+    zoom: 3,
+    extent,
+  });
+
+  return (
+    <div className={styles.map} ref={mapElement}/>
+  );
+}
+
+export default Map;
